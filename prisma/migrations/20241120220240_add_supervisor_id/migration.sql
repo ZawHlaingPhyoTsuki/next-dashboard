35e9c83fe4fef1ed544b601cece476d7f5334fc6
ZawHlaingPhyoTsuki <tro2233zhp@gmail.com>
2024-11-25 12:19:34 +0700

    first commit

---
+++ prisma/migrations/20241120220240_add_supervisor_id/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `supervisorid` on the `Class` table. All the data in the column will be lost.
+  - You are about to drop the column `endDate` on the `Event` table. All the data in the column will be lost.
+  - You are about to drop the column `startDate` on the `Event` table. All the data in the column will be lost.
+  - Added the required column `endTime` to the `Event` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `startTime` to the `Event` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `birthday` to the `Student` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `birthday` to the `Teacher` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Class" DROP CONSTRAINT "Class_supervisorid_fkey";
+
+-- DropIndex
+DROP INDEX "Lesson_name_key";
+
+-- AlterTable
+ALTER TABLE "Class" DROP COLUMN "supervisorid",
+ADD COLUMN     "supervisorId" TEXT;
+
+-- AlterTable
+ALTER TABLE "Event" DROP COLUMN "endDate",
+DROP COLUMN "startDate",
+ADD COLUMN     "endTime" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "startTime" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Student" ADD COLUMN     "birthday" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Teacher" ADD COLUMN     "birthday" TIMESTAMP(3) NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Class" ADD CONSTRAINT "Class_supervisorId_fkey" FOREIGN KEY ("supervisorId") REFERENCES "Teacher"("id") ON DELETE SET NULL ON UPDATE CASCADE;
